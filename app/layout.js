92c43c14ee4ff72034767235b45aac21faf3d6b3
PraveenNegi1 <praveennegi700@gmail.com>
2026-03-27 17:28:33 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-gray-100 min-h-screen">
         <AuthProvider>
-          {/* <Nav /> */}
           <main className="max-w-6xl mx-auto">{children}</main>
         </AuthProvider>
       </body>
